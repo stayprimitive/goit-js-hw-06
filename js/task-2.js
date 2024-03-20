c0915c8fc6bfcc849c8383aa88d807b5e39275b3
stayprimitive <119599068+stayprimitive@users.noreply.github.com>
2024-03-20 23:41:58 +0200

    Task-2 - DONE!

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,28 @@
-function getShippingMessage(country, price, deliveryFee) {
-    let totalPrice = price + deliveryFee;
-    return `Shipping to ${country} will cost ${totalPrice} credits`
+class Storage {
+    #items;
+    constructor(items) {
+        this.#items = items;
+    }
+
+    getItems() {
+        return this.#items;
+    }
+    
+    addItem(newItem) {
+        this.#items.push(newItem)
+    }
+
+    removeItem(itemToRemove) {
+        if (this.#items.includes(itemToRemove)) {
+                  this.#items.splice(this.#items.indexOf(itemToRemove), 1);
+        }
+        return this.#items;
+    }
 }
 
-console.log(getShippingMessage("Australia", 120, 50));
-console.log(getShippingMessage("Germany", 80, 20));
-console.log(getShippingMessage("Sweden", 100, 20));
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
